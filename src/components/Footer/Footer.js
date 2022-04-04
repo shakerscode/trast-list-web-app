1cd9042d011d280b6f90cfa11efac057b7db6aa4
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-04 23:44:50 +0600

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer-sec'>
+            <h6>All Right Reserved © Copyright Shaker Ahamed</h6>
+        </div>
+    );
+};
+
+export default Footer;
