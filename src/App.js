c2748ed3de18ee94d5ec3412236e00c720b607f9
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-04 16:20:50 +0600

    blog section is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AllReviews from './components/AllReviews/AllReviews';
+import Blog from './components/Blog/Blog';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/reviews' element={<AllReviews></AllReviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='*' element = {<NotFound></NotFound>}></Route>
       </Routes>
     </div>
---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <div className='blog-section'>
+
+            <div>
+                <h1>What is context API?</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae error asperiores praesentium tenetur deserunt molestias iure nihil expedita mollitia odit.</p>
+            </div>
+            <div>
+                <h1>What is Semantic Tag?</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt delectus quia inventore neque sit dignissimos iusto natus architecto fugiat laudantium!</p>
+            </div>
+            <div>
+                <h1>What is the different between inline block and inline block elements?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam omnis natus adipisci porro sed, placeat consequuntur aliquam rem ab.</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Blog;
